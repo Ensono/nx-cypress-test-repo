00d621bcee6a4de6d27e06579d97fe84438d8f54
Daniel Phillips <daniel.phillips@ensono.com>
2023-05-22 12:42:01 +0100

    Baseline

---
+++ apps/next-application-for-e2e/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'next-application-for-e2e',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/next-application-for-e2e',
+};
